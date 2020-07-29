7dc396b040a333b310a6abbff948517a76c64532
Mike Nunan <mike.nunan@bp.com>
2020-07-29 15:38:57 +0100

    Remove unneeded conditional compilation

--- Dasher.Tests/TestUtils.cs
+++ Dasher.Tests/TestUtils.cs
@@ -31,11 +31,7 @@ namespace Dasher.Tests
         public static void CleanUpForPerfTest()
         {
             GC.Collect(2, GCCollectionMode.Forced);
-
-#if !NETCOREAPP3_1
             GC.WaitForFullGCComplete(1000);
-#endif
-
             GC.WaitForPendingFinalizers();
         }
     }
